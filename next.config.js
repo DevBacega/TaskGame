77a65dd4f71137c55d879dccdf7970bc0d14c30e
Victor Bacega <victor.faria@raizen.com>
2022-09-13 21:46:38 -0300

    Teste with var 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    BASE_URL: !!process.env.VERCEL_URL ? `${process.env.VERCE_URL}/api` : 'http://localhost:3000/api',
+    BASE_URL: !!process.env.VERCEL_URL ? '/api' : 'http://localhost:3000/api',
   },
   
 }
